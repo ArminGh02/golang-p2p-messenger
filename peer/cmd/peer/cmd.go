b8a4830e28d3acaf484703bfe7000a92f8463c36
Armin Gh <arminghorbanian02@gmail.com>
2023-06-01 23:40:48 +0330

    Root command

--- peer/cmd/peer/cmd.go
+++ peer/cmd/peer/cmd.go
@@ -8,26 +8,22 @@ import (
 	"net"
 	"os"
 
-	"github.com/mitchellh/go-homedir"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/ArminGh02/golang-p2p-messenger/internal/protocol"
-	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/get"
-	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/list"
-	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/send"
-	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/start"
+	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/root"
 )
 
 var (
 	logger *logrus.Logger
 
-	tcpPort  uint16
-	udpPort  uint16
-	cfgFile  string
-	username string
+	tcpPort uint16
+	udpPort uint16
+	cfgFile string
 )
 
 func initConfig() {
@@ -53,29 +49,17 @@ func NewCommand() *cobra.Command {
 	cobra.OnInitialize(initConfig)
 
 	cmd := &cobra.Command{
-		Use:     "peer [flags]",
-		Short:   "Command line p2p messenger",
-		PreRunE: preRun,
-		Run:     run,
+		Use:   "messenger",
+		Short: "Command line p2p messenger",
+		RunE:  run,
 	}
 
 	cmd.Flags().Uint16VarP(&udpPort, "tcp-port", "t", 8081, "TCP port to listen on")
 	cmd.Flags().Uint16VarP(&tcpPort, "udp-port", "u", 8082, "UDP port to listen on")
 	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/config.yaml and current directory)")
-	cmd.PersistentFlags().StringVarP(&username, "username", "n", "", "username to use")
-	cmd.PersistentFlags().StringP("server", "s", "http://localhost:8080", "server to connect to")
 
 	viper.BindPFlag("tcp-port", cmd.Flags().Lookup("tcp-port"))
 	viper.BindPFlag("udp-port", cmd.Flags().Lookup("udp-port"))
-	viper.BindPFlag("username", cmd.PersistentFlags().Lookup("username"))
-	viper.BindPFlag("server", cmd.PersistentFlags().Lookup("server"))
-
-	cmd.AddCommand(
-		start.NewCommand(), // start connection to stun
-		list.NewCommand(),  // list all peers
-		get.NewCommand(),   // get peer by username
-		send.NewCommand(),  // send image/text to a peer
-	)
 
 	logger = logrus.New()
 	logger.Out = cmd.OutOrStdout()
@@ -83,47 +67,7 @@ func NewCommand() *cobra.Command {
 	return cmd
 }
 
-func run(cmd *cobra.Command, args []string) {
-	for {
-		select {
-		case <-cmd.Context().Done():
-			return
-		default:
-			selectedCmd := promptCommand(cmd)
-			if selectedCmd == nil {
-				cmd.Println("Invalid command\n" + cmd.UsageString())
-				continue
-			}
-			if err := selectedCmd.Execute(); err != nil {
-				logger.Errorln("Error executing command:", "error", err)
-			}
-		}
-	}
-}
-
-func promptCommand(rootCmd *cobra.Command) *cobra.Command {
-	for _, cmd := range rootCmd.Commands() {
-		rootCmd.Printf("%s: %s\n", cmd.Name(), cmd.Short)
-	}
-
-	rootCmd.Print("Enter command: ")
-	var cmdName string
-	fmt.Scan(&cmdName)
-
-	for _, cmd := range rootCmd.Commands() {
-		if cmd.Name() == cmdName {
-			return cmd
-		}
-	}
-
-	return nil
-}
-
-func preRun(cmd *cobra.Command, args []string) error {
-	if username == "" {
-		return fmt.Errorf("username is required")
-	}
-
+func run(cmd *cobra.Command, args []string) error {
 	txtChan := make(chan string)
 	imgChan := make(chan image.Image)
 
@@ -131,12 +75,26 @@ func preRun(cmd *cobra.Command, args []string) error {
 	defer close(imgChan)
 
 	go loopPrintOutput(cmd.Context(), cmd.OutOrStderr(), txtChan, imgChan)
+	go loopRunCommand(cmd.Context())
 
 	group, ctx := errgroup.WithContext(cmd.Context())
 	group.Go(func() error { return loopReceiveText(ctx, txtChan) })
 	group.Go(func() error { return loopReceiveImage(ctx, imgChan) })
 
 	return group.Wait()
+}
+
+func loopRunCommand(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err := root.NewCommand().Execute(); err != nil {
+				logger.Errorln("Error executing command:", "error", err)
+			}
+		}
+	}
 }
 
 func loopReceiveText(ctx context.Context, out chan<- string) error {
---
+++ peer/cmd/peer/root/cmd.go
@@ -0,0 +1,39 @@
+package root
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/get"
+	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/list"
+	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/send"
+	"github.com/ArminGh02/golang-p2p-messenger/peer/cmd/peer/start"
+)
+
+var logger *logrus.Logger
+
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "peer <command> [options]",
+		Short: "Command line p2p messenger",
+	}
+
+	cmd.PersistentFlags().StringP("username", "n", "", "username to use")
+	cmd.PersistentFlags().StringP("server", "s", "http://localhost:8080", "server to connect to")
+
+	viper.BindPFlag("username", cmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("server", cmd.PersistentFlags().Lookup("server"))
+
+	cmd.AddCommand(
+		start.NewCommand(), // start connection to stun
+		list.NewCommand(),  // list all peers
+		get.NewCommand(),   // get peer by username
+		send.NewCommand(),  // send image/text to a peer
+	)
+
+	logger = logrus.New()
+	logger.Out = cmd.OutOrStdout()
+
+	return cmd
+}
