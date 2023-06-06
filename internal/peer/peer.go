2f545d5611582d1f56d5131a2862fd30f4099c79
Armin Gh <arminghorbanian02@gmail.com>
2023-06-07 00:12:32 +0330

    Implement `Stringer` for `peer.Peer`

--- internal/peer/peer.go
+++ internal/peer/peer.go
@@ -1,7 +1,13 @@
 package peer
 
+import "fmt"
+
 type Peer struct {
 	UDPAddr  string `json:"udp_addr"`
 	TCPAddr  string `json:"tcp_addr"`
 	Username string `json:"username"`
+}
+
+func (p *Peer) String() string {
+	return fmt.Sprintf("Peer{Username:%s, UDPAddr:%s, TCPAddr:%s}", p.Username, p.UDPAddr, p.TCPAddr)
 }
